63ca0f0e779d38300dc9b8831c59bc25ba14ed6f
dedekharisma <dekharisma@gmail.com>
2025-07-21 17:08:39 +0700

    Update

---
+++ day_1/nickname.py
@@ -0,0 +1,4 @@
+print("Welcome to the Nickname Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your nick name could be: " + city + " " + pet)
---
+++ day_1/variables.py
@@ -0,0 +1,19 @@
+"""
+name =  input("What is your name?")
+string_length = len(name)
+print(len(name))
+"""
+from sympy.codegen.fnodes import use_rename
+from sympy.solvers.diophantine.diophantine import length
+
+# print(len(input("What is your name?")))
+
+username = input("username")
+length =len(username)
+print(length)
+
+# print(len(username))
+
+glass1 = "milk"
+glass2 = "juice"
+glass1 = glass2
