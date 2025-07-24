2f9548fb68072b8c58c099d29f33cadcc9ca831b
dedekharisma <dekharisma@gmail.com>
2025-07-24 08:24:54 +0700

    Update

---
+++ day_2/data_types.py
@@ -0,0 +1,26 @@
+"""
+Primitive data types in Python
+1. Strings
+2. Integers
+3. Floats
+4. Booleans
+"""
+# Subscripting
+print("Hello"[4])
+print("Hello"[-1])
+
+# String
+print("123" + "345")
+
+# Integer = whole number
+print(123 + 345)
+
+# Large Integer
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
