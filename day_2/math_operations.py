01f991e6a336dc9cce33a90d4ccd3b4491dc5d52
dedekharisma <dekharisma@gmail.com>
2025-07-24 16:51:25 +0700

    add math operations

---
+++ day_2/math_operations.py
@@ -0,0 +1,32 @@
+print("My age: " + str(20))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(5 / 3)
+print(5 // 3)
+print(2 ** 3)
+
+# Priority (PEMDAS)
+# ()
+# **
+# *
+# /
+# +
+# -
+#
+# * = /
+#
+# + = -
+
+print(3 * 3 + 3 / 3 - 3)
+print(3/3*3+3-3)
+print(3 * (3 + 3) / 3 - 3)
+
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / height ** 2
+
+print(bmi)
