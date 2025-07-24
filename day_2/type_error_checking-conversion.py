f59568b0f8bb31ed302d5cb4a11a2c74a1d4039a
dedekharisma <dekharisma@gmail.com>
2025-07-24 08:43:35 +0700

    Update

---
+++ day_2/type_error_checking-conversion.py
@@ -0,0 +1,15 @@
+"""
+print(type("abcd"))
+print(type(123456))
+print(type(3.14))
+print(type(True))
+"""
+# Type Conversion
+"""
+int()
+float()
+str()
+bool()
+"""
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
